ddc1659a9e72356bd97b4f0c39c762481c64ba3c
Kieu Thi Kim Phuong <phuongkimthikieu@gmail.com>
2023-10-21 08:47:00 +0700

    Cập nhập code cơ bản

---
+++ QuanLyThuVien/DTO/DAO/DataProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.DAO
+{
+    public class DataProvider
+    {
+        //Tao chuoi ket noi co so du lieu
+
+        static string provider = "Data Source=LAPTOP-J07245G8\\SQLEXPRESS;Initial Catalog=QLThuVien;Integrated Security=True";
+        SqlConnection connect = new SqlConnection(provider);
+
+        public DataTable GetData(string sql)
+        {
+            DataTable rs = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(sql, connect);
+            adapter.Fill(rs);
+            return rs;
+        }
+
+        public void Excute(string sql)
+        {
+            connect.Open();
+            SqlCommand command = new SqlCommand(sql, connect);
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+    }
+
+}
